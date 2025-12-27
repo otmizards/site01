6c07d2fe118cbbb638a4387547dd79b574c1c412
Felipe <adm.otmizards@gmail.com>
2025-12-27 14:32:09 +0000

    template: config central + home dinâmica

--- site01/astro.config.mjs
+++ site01/astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://site01.360winspt.com",
+  site: "https://site01-4im.pages.dev",
   integrations: [sitemap()],
 });
